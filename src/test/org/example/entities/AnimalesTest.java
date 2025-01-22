9aceb1585f1ef3e3e9c372cf83d0d40398f3f935
josemanuel200502 <145467931+josemanuel200502@users.noreply.github.com>
2025-01-22 17:47:21 +0100

    Se han creado los test y se han tocado las clases y el hibernate.xml

---
+++ src/test/org/example/entities/AnimalesTest.java
@@ -0,0 +1,63 @@
+package org.example.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AnimalesTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getNombre() {
+    }
+
+    @Test
+    void setNombre() {
+    }
+
+    @Test
+    void getEspecie() {
+    }
+
+    @Test
+    void setEspecie() {
+    }
+
+    @Test
+    void getEdad() {
+    }
+
+    @Test
+    void setEdad() {
+    }
+
+    @Test
+    void getDescripcion() {
+    }
+
+    @Test
+    void setDescripcion() {
+    }
+
+    @Test
+    void getEstado() {
+    }
+
+    @Test
+    void setEstado() {
+    }
+
+    @Test
+    void getFamilia() {
+    }
+
+    @Test
+    void setFamilia() {
+    }
+}
